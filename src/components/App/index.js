a79558c2254b63ef5a350c517839840a378c460b
federikos <federikos@bk.ru>
2019-09-23 15:46:43 +0300

    application routing setting

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
+import HomePage from '../HomePage';
+import Dashboard from '../Dashboard';
+import Register from '../Register';
+import Login from '../Login';
+import {MuiThemeProvider,createMuiTheme} from '@material-ui/core/styles';
+import {CssBaseline} from '@material-ui/core';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+
+const theme=createMuiTheme()
+
+/*It is created as a component function in the react hooks.*/
 function App(props){
     return(
-        <div>
-            App Component
-        </div>
+       <MuiThemeProvider theme={theme}>
+           <CssBaseline/>
+           <Router>
+               <Switch>
+                   {/* Routing according to the path entered */}
+                   <Route exact path='/' component={HomePage} />
+                   <Route exact path='/register' component={Register} />
+                   <Route exact path='/login' component={Login} />
+                   <Route exact path='/dashboard' component={Dashboard} />
+               </Switch>
+           </Router>
+       </MuiThemeProvider>
     )
 }
 
-export default App
+export default App /*we export to access other files.*/
