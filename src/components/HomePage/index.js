eae96f7d58fe20780ef0bfba42034ed1a09c73d6
federikos <federikos@bk.ru>
2019-09-28 17:46:21 +0300

    create homepage page

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,75 @@
 import React from 'react';
+import { Typography, Paper, Avatar, Button} from '@material-ui/core'
+import VerifiedUserOutlined from '@material-ui/icons/VerifiedUserOutlined'
+import withStyles from '@material-ui/core/styles/withStyles'
 
-function HomePage(props){
-    return(
-        <div>
-        HomePage Component
-        </div>
+const styles = theme => ({
+    main: {
+        width: 'auto',
+        display: 'block', 
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.primary.main,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
+})
+
+function HomePage(props) {
+    const { classes } = props
+
+    return (
+        <main className={classes.main}>
+            <Paper className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <VerifiedUserOutlined />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Hi Steemit User
+                </Typography>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="outlined"
+                    color="primary"
+                    className={classes.submit}>
+                    Register
+                </Button>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}>
+                    Login
+                </Button>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="secondary"
+                    className={classes.submit}>
+                    Dashboard
+                </Button>
+            </Paper>
+        </main>
     )
 }
 
-export default HomePage
+export default withStyles(styles)(HomePage)
